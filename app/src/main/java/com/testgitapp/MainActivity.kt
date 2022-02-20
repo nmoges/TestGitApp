559a630f94da07c0baaf4e132c7bb84bf350d03b
nmoges <nicolasmoges@gmail.com>
2022-02-20 10:20:19 +0100

    Add new button to MainActivity

--- app/src/main/java/com/testgitapp/MainActivity.kt
+++ app/src/main/java/com/testgitapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.testgitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Log.d("BUTTON", "CLick detected")
+        }
     }
 }
